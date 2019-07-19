71c49433d2cbc9fcc791de6c966c4bc0b9c0b17d
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 20:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-funtrion happyHolidayTo(holiday,name)
+function happyHolidayTo(holiday,name)
 {
   return `Happy ${holiday}, ${name}!`;
 }
